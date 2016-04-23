4a0f565ab8f36fe74b47d467bec708d4ca44c1c1
Andrey Chugay <kamaelua@live.com>
2016-04-23 18:24:13 +0300

    RC 2

---
+++ AudioStream/Connection.cs
@@ -0,0 +1,31 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioStream
+{
+    class Connection
+    {
+        WaveInEvent waveIn;
+
+        public TcpClient cl;
+
+        public void Work()
+        {           
+            waveIn = new WaveInEvent();
+            
+            waveIn.WaveFormat = WaveFormat.CreateIeeeFloatWaveFormat(44100, 2);
+            waveIn.DataAvailable += InputBufferToFileCallback;
+            waveIn.StartRecording();
+        }
+
+        private void InputBufferToFileCallback(object sender, WaveInEventArgs e)
+        {
+            cl.Client.SendTo(e.Buffer, cl.Client.RemoteEndPoint);
+        }
+    }
+}
--- AudioStream/MainWindow.xaml.cs
+++ AudioStream/MainWindow.xaml.cs
@@ -25,60 +25,34 @@ namespace AudioStream
     /// </summary>
     public partial class MainWindow : Window
     {
-        Thread audioStream;
-        WasapiLoopbackCapture capture;
-        Socket outputSocket;
-        IPEndPoint ep;
-        byte[] buffer;
-        int bytesRecorded;
         public MainWindow()
         {
-            //outputSocket = new Socket(AddressFamily.InterNetwork, );
-            IPAddress broadcast = IPAddress.Parse("192.168.0.101");
-            ep = new IPEndPoint(broadcast, 11001);
-
-            audioStream = new Thread(SendUdp);
-            capture = new WasapiLoopbackCapture();
-            capture.WaveFormat = WaveFormat.CreateIeeeFloatWaveFormat(44100, 2);
-            capture.DataAvailable += InputBufferToFileCallback;
-            capture.ShareMode = NAudio.CoreAudioApi.AudioClientShareMode.Shared;
             
-            capture.StartRecording();
-            InitializeComponent();
-            GetAudio();
         }
 
-        public async void GetAudio()
+        public void StartServer()
         {
+            int port = 11001;
+            TcpListener l = new TcpListener(port); //и на нем у нас висит сервер
+            l.Start();
 
-            
+            Console.WriteLine("The server is started..."); //уведомили об этом
 
-            //WasapiOut wsout = new WasapiOut()
+            while (true)
+            {
+                TcpClient cl = l.AcceptTcpClient();
+                Connection c = new Connection();
+                c.cl = cl;
+                Thread t = new Thread(new ThreadStart(c.Work));
+                t.IsBackground = false;
+                t.Start();
+            }
         }
 
-        private void InputBufferToFileCallback(object sender, WaveInEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            outputSocket.SendTo(e.Buffer, ep);
-            
-            //audioStream.Start();
+            new Thread(StartServer).Start();
+            BtnStart.IsEnabled = false;
         }
-
-        public void SendUdp()
-        {
-            
-
-            
-
-            //Console.WriteLine("Message sent to the broadcast address");
-            //audioStream.Suspend();
-        }
-
-        public void soc()
-        {
-            
-
-
-        }
-
     }
 }
